60d171dc3743e7cddc5303e5722021e609495d68
Gergő Gazda <info@gazdagergo.com>
2020-02-04 05:50:24 +0100

    no service worker

--- next.config.js
+++ next.config.js
@@ -2,30 +2,30 @@
 // Network-first strategy means that if there is no internet connection,
 // the browser will use files previously saved locally to the user’s device instead.
 // AKA Offline Mode!
-const withOffline = require("next-offline");
+// const withOffline = require("next-offline");
 
-const nextConfig = {
-  target: "serverless",
-  workboxOpts: {
-    swDest: "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "networkFirst",
-        options: {
-          cacheName: "https-calls",
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }
-    ]
-  }
-};
+// const nextConfig = {
+//   target: "serverless",
+//   workboxOpts: {
+//     swDest: "static/service-worker.js",
+//     runtimeCaching: [
+//       {
+//         urlPattern: /^https?.*/,
+//         handler: "networkFirst",
+//         options: {
+//           cacheName: "https-calls",
+//           networkTimeoutSeconds: 15,
+//           expiration: {
+//             maxEntries: 150,
+//             maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+//           },
+//           cacheableResponse: {
+//             statuses: [0, 200]
+//           }
+//         }
+//       }
+//     ]
+//   }
+// };
 
-module.exports = withOffline(nextConfig);
+// module.exports = withOffline(nextConfig);
